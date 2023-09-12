408825d9e37e8501f0dc06384ecd546626cdc02e
Nassahel Elias <79935315+nassahel@users.noreply.github.com>
2023-09-12 13:36:00 -0300

    se agregaron las secciones de la pagina

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Footer from './assets/components/footer/Footer'
+import Nav from './assets/components/nav/Nav'
+import Section01 from './assets/components/section01/Section01'
+import Section02 from './assets/components/section02/Section02'
+import Section03 from './assets/components/section03/Section03'
+import Section04 from './assets/components/section04/Section04'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+    <Nav/>
+    <Section01/>
+    <Section02/>
+    <Section03/>
+    <Section04/>
+    <Footer/>
+
+    </div>
   )
 }
 
---
+++ src/assets/components/section02/Section02.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Section02() {
+  return (
+    <div>Section02</div>
+  )
+}
+
+export default Section02
