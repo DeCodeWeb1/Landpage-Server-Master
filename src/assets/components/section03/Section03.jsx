b4f23e6ed623d1f94ef83f0b5e7adcbf686bdaf4
luischehin <luischehinok@gmail.com>
2023-09-13 20:01:21 -0300

    empece la creacion de contactos

--- src/assets/components/section03/Section03.jsx
+++ src/assets/components/section03/Section03.jsx
@@ -1,15 +1,30 @@
 import React from 'react'
 import '../css/general.css'
 import Footer from '../footer/Footer'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
 
 function Section03() {
   return (
     <div className='seccion3'>
-      <p>contacto. numero de telefono, direccion. tewefonos email.formularuio??? </p>
-
-
-
-      <Footer />
+      <h2 className='contacto'>Contacto</h2>
+      <div className='contenedor'>
+        <Container>
+          <Row>
+            <Col className='iconos1'>
+              <FontAwesomeIcon icon={faPhone} className='phone' />
+              <h4>381-5906929</h4>
+            </Col>
+            <Col className='iconos2'>
+              <FontAwesomeIcon icon={faEnvelope} className='mail' />
+              <h4>servermasterssas@gmail.com</h4>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </div>
   )
 }
