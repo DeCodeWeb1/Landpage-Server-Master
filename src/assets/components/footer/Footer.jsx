505d2e056c0fe4f5726b648a0925cdd5236b01c2
Pia Lopez <ppialopez@gmail.com>
2023-09-15 20:49:45 -0300

    se agrego footer

--- src/assets/components/footer/Footer.jsx
+++ src/assets/components/footer/Footer.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-import '../css/general.css'
+import './footer.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
+import logoSM from '../../img/logo transparente.png'
 
 function Footer() {
   return (
-    <footer>Footer</footer>
+    <footer className='footer'>
+      <div className='contenedor-footer d-flex align-items-center justify-content-around align-items-center pt-4'>
+      <img src={logoSM} alt="logo server master" className='logo-server' />
+      <p className='text-footer'>Todos los derechos reservados</p>
+      <p lassName='text-footer p-5'> <span><FontAwesomeIcon icon={faPhone} className='phone' /></span> 381-5906929</p>
+      </div>
+    </footer>
   )
 }
 
