0f4bdef18efee378e47bee89b2885550d4aab356
luischehin <luischehinok@gmail.com>
2023-09-17 12:04:54 -0300

    prototipo

--- src/assets/components/section01/Section01.jsx
+++ src/assets/components/section01/Section01.jsx
@@ -6,17 +6,17 @@ import imagenServer from '../../img/img-server-master.jpg'
 
 
 function Section01() {
-  return (
-    <>
+	return (
+		<>
 
-    <div className="contenedor-main">
-      <div className="contenedor-principal border border-5 d-flex flex-column align-items-center">
-        <h1 className="titulo-principal fs-1 mt-2">Server Master</h1>
-        <div className="contenedor-texto">
-          <p className="text-acerca mt-1 fs-5 text-start">Somos una empresa dedicada al mantenimiento industrial, que prestar sus servicios de manera agil y garantizada para cumplir con las necesidades de nuestros clientes</p>
-          <p className="text-acerca mt-1 fs-5 text-start">Con personal capacitado, brindamos garantia y soluciones definitivas con nuestro servicio</p>
-        </div>
-        {/* 			<h2 className="h2-mecanica">Division Mecanica Industrial</h2>
+			<div className="contenedor-main seccionUno">
+				<div className="contenedor-principal border border-5 d-flex flex-column align-items-center">
+					<h1 className="titulo-principal fs-1 mt-2">Server Master</h1>
+					<div className="contenedor-texto">
+						<p className="text-acerca mt-1 fs-5 text-start">Somos una empresa dedicada al mantenimiento industrial, que prestar sus servicios de manera agil y garantizada para cumplir con las necesidades de nuestros clientes</p>
+						<p className="text-acerca mt-1 fs-5 text-start">Con personal capacitado, brindamos garantia y soluciones definitivas con nuestro servicio</p>
+					</div>
+					{/* 			<h2 className="h2-mecanica">Division Mecanica Industrial</h2>
 			<div className="contenedor-mecanica-industrial">
 				<p>Mantenenimiento de compresores de aire</p>
 				<p>Mantenenimiento de bombas de agua y fluidos especiales</p>
@@ -35,13 +35,13 @@ function Section01() {
 				<p>Reparacion de placas electronicas</p>
 				</div>
 			</div> */}
-        <div className="contenedor-img">
-          <img src={imagenServer} alt="Imagen de mantenimiento" className="img-fluid imagen-mantenimiento rounded" />
-        </div>
-      </div>
-    </div>
-    </>
-  )
+					<div className="contenedor-img">
+						<img src={imagenServer} alt="Imagen de mantenimiento" className="img-fluid imagen-mantenimiento rounded" />
+					</div>
+				</div>
+			</div>
+		</>
+	)
 }
 
 export default Section01
