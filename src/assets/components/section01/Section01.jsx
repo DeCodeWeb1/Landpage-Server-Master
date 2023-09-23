5925b9f385e0b79f7d6f1f8c421a5ab53d86d6b7
Pia Lopez <ppialopez@gmail.com>
2023-09-22 22:29:14 -0300

    arregle el responsive

--- src/assets/components/section01/Section01.jsx
+++ src/assets/components/section01/Section01.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './section01.css'
-import imagenServer from '../../img/img-server-master.jpg'
 
 
+function Section01() {
+    return (
+            <div className="contenedor-main" id="home">
+                <div className="contenedor-principal d-flex align-items-center">
+                    <div className="contenedor-texto ml-5">
+                        <h1 className="titulo-principal fs-1 mb-1"><span className='server'>Serve </span> Master S.A.S</h1>
+                        <h2 className="subtitulo-principal fs-4 mb-1">Servicios de mantenimiento industrial</h2>
+                        <p className="text-acerca mt-1 fs-5">Somos una empresa dedicada al mantenimiento industrial, que presta sus servicios de manera ágil y garantizada para cumplir con las necesidades de nuestros clientes</p>
+                        <p className="text-acerca mt-1 fs-5">Con personal capacitado, brindamos garantía y soluciones definitivas con nuestro servicio</p>
+                    </div>
+                </div>
+            </div>
+    )
+}
 
+export default Section01;
+
+/* 
 function Section01() {
 	return (
 		<>
@@ -21,4 +37,4 @@ function Section01() {
 	)
 }
 
-export default Section01
+export default Section01 */
