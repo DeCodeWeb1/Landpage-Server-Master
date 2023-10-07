cb6d33dbdd499b8416ace34336c17c5a6104f1e3
Nassahel Elias <79935315+nassahel@users.noreply.github.com>
2023-10-06 23:24:31 -0300

    se corrigio responsive del formulario

--- src/assets/components/section03/Section03.jsx
+++ src/assets/components/section03/Section03.jsx
@@ -7,34 +7,34 @@ import Form from 'react-bootstrap/Form';
 
 function Section03() {
   return (
-    <div className='w-100 seccion3 mb-4' id='contact' >
-      <Form>
-        <div className='text-center w-100 mt-5 mb-5 h1-left-align'>
-          <h1 className='contactanos'>Contactanos</h1>
-        </div>
-        <Form.Group className="mb-3 w-25 mx-auto " controlId="exampleForm.ControlInput1">
+    <div className=' seccion3 mb-4' id='contact' >
+      <div className='mt-5 mb-5 '>
+        <h1 className='contactanos text-start ms-3'>Contáctanos</h1>
+      </div>
+      <Form className='col-10 col-lg-4 mx-auto'>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
           <Form.Control className='input' type="text" placeholder="Nombre" />
         </Form.Group>
 
-        <Form.Group className="mb-3 w-25 mx-auto" controlId="exampleForm.ControlInput1">
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
           <Form.Control className='input' type="email" placeholder="email@ejemplo.com" />
         </Form.Group>
 
-        <Form.Group className="mb-3 w-25 mx-auto" controlId="exampleForm.ControlInput1">
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
           <Form.Control className='input' type="text" placeholder="Asunto" />
         </Form.Group>
 
 
-        <Form.Group className="mb-5 mx-auto w-25" controlId="exampleForm.ControlInput1">
-          <Form.Label className='titulos'>Motivo</Form.Label>
-          <Form.Control className='input' as="textarea" rows={4} />
+        <Form.Group className="mb-5" controlId="exampleForm.ControlInput1">
+          <Form.Control className='input' as="textarea" rows={4} placeholder='Motivo' />
         </Form.Group>
 
-        <Form.Group controlId="formFile" className="mb-3  w-25 mx-auto">
+        <Form.Group controlId="formFile" className="mb-3 col-lg-8 mx-auto">
           <Form.Control className='input' type="file" />
         </Form.Group>
-
-        <Button className='enviar mx-auto' variant="primary">Enviar</Button>
+        <div className='text-center'>
+          <Button style={{ backgroundColor: '#3f6085ff' }} className='enviar px-4 mt-5 text-light border-light ' variant='dark'>Enviar</Button>
+        </div>
 
       </Form>
     </div>
