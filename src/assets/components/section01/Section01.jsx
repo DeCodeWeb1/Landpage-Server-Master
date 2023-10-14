ee38b50e04b5acd63b020eccc05c1858f0ddd781
Pia Lopez <ppialopez@gmail.com>
2023-10-14 01:06:58 -0300

    realizamos cambios de estilos en toda la pagina

--- src/assets/components/section01/Section01.jsx
+++ src/assets/components/section01/Section01.jsx
@@ -7,11 +7,11 @@ import logo from '../../img/logo-transparente.png'
 function Section01() {
   return (
     <div className="contenedor-main" id="home">
-      <img className='imagen-fondo' src={fondo} alt="fondo imagen mantenimiento" />
+
       <div className="contenedor-principal d-flex align-items-center">
         <div className="contenedor-texto ml-5">
-          <img className='logo-serve' src={logo} alt="logo de serve master" />
-          <h1 className="titulo-principal fs-1 mt-3"><span className='server'>SERVE</span>MASTER S.A.S</h1>
+          <img className='logo-serve img-fluid col-lg-5' src={logo} alt="logo de serve master" />
+          <h1 className="titulo-principal fs-1 mt-3 col-12"><span className='server'>SERVE</span>MASTER S.A.S</h1>
           <h2 className="subtitulo-principal fs-4 mt-3">SERVICIOS DE MANTENIMEINTO INDUSTRIAL</h2>
         </div>
       </div>
