f4ee368da04cd7a16e351fde14a91bd66e00ded7
Nassahel Elias <79935315+nassahel@users.noreply.github.com>
2023-10-29 22:47:51 -0300

    se corrigio footer

--- src/assets/components/footer/Footer.jsx
+++ src/assets/components/footer/Footer.jsx
@@ -6,13 +6,19 @@ import logoSM from '../../img/logo-transparente.png'
 
 function Footer() {
   return (
-    <footer className='footer d-flex align-items-center justify-content-around pt-4'>
-      <img src={logoSM} alt="logo server master" className='logo-server' />
-      <p className='text-footer'>Todos los derechos reservados</p>
-      <div className='d-flex gap-2 col-2'>
-        <FontAwesomeIcon icon={faPhone} className='phone' />
-        <p className='text-footer mb-0'>3865-607676</p>
+    <footer className='footer container-fluid d-flex align-items-center justify-content-around pt-4'>
+      <div className="row justify-content-around">
+        <div className="col-12 col-lg-3 text-center pb-2">
+          <img src={logoSM} alt="logo server master"  className='logo-server w-25' />
+        </div>
+        <div className="col-12 col-lg-3 text-center pb-2">
+          <p className='text-footer'>©2023 Servemaster. Todos los derechos reservados.</p>
+        </div>
+        <div className='col-12 col-lg-3 text-center pb-2'>          
+          <p className='text-footer mb-0'><FontAwesomeIcon icon={faPhone} className='phone me-2' />3865-607676</p>
+        </div>
       </div>
+
     </footer>
   )
 }
